ba4d2de13456251f27f1ac2679497b4624355cdf
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-11-24 10:50:18 +0100

    Add mainTheme

---
+++ src/styles/mainTheme.tsx
@@ -0,0 +1,15 @@
+const mainTheme = {
+  primaryColor: "#3793C2",
+  filterTextColor: "#3793C2",
+  primaryLetterColor: "#5A5B5C",
+  primaryLetterButtonColor: "#2B2A2D",
+  primaryFooterTextColor: "#B6BCBD",
+  backgroundFooterColor: "#2B2A2D",
+  mainItemTextColor: "#000",
+  changeImageMaincolor: "#3793C2",
+  modalTextMaincolor: "#3793C2",
+  errorButtonPageBackgroundcolor: "#D9D9D9",
+  errorButtonTextcolor: "#fff",
+};
+
+export default mainTheme;

48cae61f9469e48e0499a1c01a0d8fd608286b4b
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-11-26 19:21:51 +0100

    Remove button component from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Button from "./components/Button/Button";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 const App = (): JSX.Element => {
   return (
     <div className="container">
       <RegisterForm />
-      <Button />
     </div>
   );
 };
--- src/components/Button/ButtonStyled.tsx
+++ src/components/Button/ButtonStyled.tsx
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 
 const ButtonStyled = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  gap: 30px;
+  max-width: 450px;
+
   background-color: ${(props) => props.theme.colors.primaryColor};
   border-radius: 4px;
   font-size: 16px;
